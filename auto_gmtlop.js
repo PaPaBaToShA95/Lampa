abac23d637755cd92266e33ddc35f1b5827f32cd
oleksijermantraut <mawabek43@gmail.com>
2026-06-14 13:32:16 +0300

    version 1.0.1

--- auto_gmtlop.js
+++ auto_gmtlop.js
@@ -8,4 +8,26 @@
     });
 
     Lampa.Noty.show('Auto GMTLOP завантажено');
+})();(function () {
+    'use strict';
+
+    function selectGMTLOP() {
+        setTimeout(function () {
+            $('.selectbox__item').each(function () {
+                let text = $(this).text().toLowerCase();
+
+                if (text.includes('gmtlop')) {
+                    $(this).trigger('hover:enter');
+                    $(this).click();
+                    return false;
+                }
+            });
+        }, 300);
+    }
+
+    Lampa.Listener.follow('select', function () {
+        selectGMTLOP();
+    });
+
+    Lampa.Noty.show('Auto GMTLOP увімкнено');
 })();

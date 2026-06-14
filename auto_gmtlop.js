89b2986673371ad06c982d1b7da0cca43da2bed6
oleksijermantraut <mawabek43@gmail.com>
2026-06-14 14:33:58 +0300

    buttom

--- auto_gmtlop.js
+++ auto_gmtlop.js
@@ -1,33 +1,46 @@
 (function() {
-    console.log('[TEST] Патріотичний плагін завантажено');
-
-    function showNotification(msg) {
-        if (typeof Lampa !== 'undefined' && Lampa.Notification && Lampa.Notification.show) {
-            Lampa.Notification.show(msg, 5000);
-        } else {
-            alert(msg);
+    // Функція додавання кнопки в меню
+    function addPatrioticButton() {
+        if (typeof Lampa === 'undefined' || !Lampa.Menu || !Lampa.Menu.add) {
+            setTimeout(addPatrioticButton, 500);
+            return;
+        }
+        
+        Lampa.Menu.add({
+            title: '💙💛 Патріот',
+            component: 'patriotic',
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FFD700"><path d="M12 2L15 8H9L12 2Z M12 8V22 M10 13L12 15L14 13"/></svg>',
+            onclick: function() {
+                if (Lampa.Notification && Lampa.Notification.show) {
+                    Lampa.Notification.show('Слава Україні! Героям слава! 💙💛', 5000);
+                } else {
+                    alert('Слава Україні! 💙💛');
+                }
+            }
+        });
+        
+        console.log('[Patriot] Кнопку додано в меню');
+        if (Lampa.Notification && Lampa.Notification.show) {
+            Lampa.Notification.show('Патріотичний плагін активовано!', 4000);
         }
     }
-
-    function init() {
-        console.log('[TEST] Init виконано');
-        showNotification('💙💛 Патріотичний плагін активовано! Слава Україні!');
-    }
-
-    // Запускаємо після повної готовності Lampa
-    if (typeof Lampa !== 'undefined') {
+    
+    // Запуск після готовності Lampa
+    function start() {
         if (window.appready) {
-            init();
+            addPatrioticButton();
         } else {
             Lampa.Listener.follow('app', function(e) {
-                if (e.type === 'ready') init();
+                if (e.type === 'ready') addPatrioticButton();
             });
         }
+    }
+    
+    if (typeof Lampa !== 'undefined') {
+        start();
     } else {
-        // Якщо Lampa ще не підвантажилась – чекаємо
         window.addEventListener('load', function() {
-            if (typeof Lampa !== 'undefined') init();
-            else console.warn('[TEST] Lampa не знайдено');
+            if (typeof Lampa !== 'undefined') start();
         });
     }
 })();
